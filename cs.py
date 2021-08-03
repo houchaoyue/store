580725f7eff94905f037a843fd390e8b6a47a890
unknown <1061892410@qq.com>
2021-08-03 17:22:11 +0800

    低版本

---
+++ cs.py
@@ -0,0 +1,27 @@
+# 这是一个示例 Python 脚本。
+
+# 按 Shift+F10 执行或将其替换为您的代码。
+# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+
+def print_hi(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+import random
+computer=random.randint(1,100)
+while True:
+    number=int(input("请输入一百以内的整数："))
+    if(number>computer):
+        print("大了")
+    elif(number<computer):
+        print("小了")
+    else:
+        print("恭喜你赢了")
+        break
